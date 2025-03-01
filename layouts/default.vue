9b13ef7e0d6d869c39d38a040dea4a2cb73ae2a9
sbastin1 <sebastian.wilden@outlook.com>
2025-03-01 05:45:36 +0100

    feat: add navbar && scss feats && pages

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,7 +1,28 @@
 <template>
-  <div></div>
+  <div class="container">
+    <OrgNavbar />
+    <slot />
+  </div>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import OrgNavbar from "~/components/organisms/OrgNavbar.vue";
+</script>
 
-<style scoped lang="scss"></style>
+<style lang="scss">
+body {
+  margin: 0;
+  padding: 0;
+}
+
+:root {
+  background-color: $background;
+  font-family: Roboto, serif;
+  color: white;
+}
+
+.container {
+  margin: auto;
+  max-width: 1280px;
+}
+</style>
