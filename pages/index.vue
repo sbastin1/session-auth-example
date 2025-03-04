e4601f105b5c506d8b1acae94537a8b0ceaee047
sbastin1 <sebastian.wilden@outlook.com>
2025-03-04 19:13:19 +0100

    feat: create homepage

--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,22 @@
 <template>
-  <div></div>
+  <div>
+    <h1>Homepage</h1>
+    <h2>very cool subtext</h2>
+  </div>
 </template>
 
 <script setup lang="ts"></script>
 
-<style scoped lang="scss"></style>
+<style scoped lang="scss">
+div {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  margin-top: 50px;
+
+  h2 {
+    color: rgb(192, 192, 192);
+  }
+}
+</style>
