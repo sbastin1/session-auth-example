2dbbdb2fbd5e95035646e9f651c784342a26ff0e
sbastin1 <sebastian.wilden@outlook.com>
2025-03-04 19:14:49 +0100

    chore: directory refactor

---
+++ app/components/atom/BaseButton.vue
@@ -0,0 +1,23 @@
+<template>
+  <button>
+    {{ text }}
+  </button>
+</template>
+
+<script setup lang="ts">
+defineProps({
+  text: {
+    type: String,
+    default: "Button",
+  },
+});
+</script>
+
+<style scoped lang="scss">
+button {
+  background-color: $primary_accent;
+  border: 0;
+  padding: 15px;
+  border-radius: 4px;
+}
+</style>
